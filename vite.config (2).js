9636db25242f4257849389c28ede2429d934cd81
LAXMI SECURITY SYSTEMS AND ELECTRICALS <laxmicctvelectricals@gmail.com>
2025-05-16 22:20:57 +0530

    Delete vite.config (2).js

--- vite.config (2).js
+++
@@ -1,8 +0,0 @@
-// vite.config.js
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  plugins: [react()],
-  base: '/Networking-Report/', // Must match your GitHub repo name (case-sensitive)
-})
