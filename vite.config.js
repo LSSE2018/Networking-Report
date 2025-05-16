8192d184d29c5198dcace1e46d71791223e55e28
LAXMI SECURITY SYSTEMS AND ELECTRICALS <laxmicctvelectricals@gmail.com>
2025-05-16 22:00:00 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,1 +1,8 @@
+// vite.config.js
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+export default defineConfig({
+  plugins: [react()],
+  base: '/Networking-Report/', // Must match your GitHub repo name (case-sensitive)
+})
